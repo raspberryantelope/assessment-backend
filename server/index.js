6cbcc77322d29399106a7c0fed9a86d5da0b3802
Alex Sutton <mandarinfish27@aol.com>
2023-11-16 17:17:07 -0700

    revised deployability

--- server/index.js
+++ server/index.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
-let path = require("path");
+
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "client")));
+app.use(express.static(`${__dirname}/client`));
 
 const { getCompliment, getFortune, receivePraise, becomeAllstar, deleteAllstar } = require('./controller');
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "index.html"));
-});
 app.get("/api/compliment", getCompliment);
 app.get("/api/fortune", getFortune)
 app.post("/api/praise", receivePraise)
 app.put("/api/allstars", becomeAllstar)
 app.delete("/api/allstars/:id", deleteAllstar)
+
 
 app.listen(4000, () => console.log("Server running on 4000"));
