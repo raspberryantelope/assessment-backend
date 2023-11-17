536d7629d23933acd4371845f5372904bc6759b3
Alex Sutton <mandarinfish27@aol.com>
2023-11-16 17:11:14 -0700

    updated again

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ const app = express();
 let path = require("path");
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "client")));
 
 const { getCompliment, getFortune, receivePraise, becomeAllstar, deleteAllstar } = require('./controller');
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public", "index.html"));
-});
+});*/
 app.get("/api/compliment", getCompliment);
 app.get("/api/fortune", getFortune)
 app.post("/api/praise", receivePraise)
