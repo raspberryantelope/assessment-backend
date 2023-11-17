1a82138d519af65504c1408da7b5ce339fc34aec
Alex Sutton <mandarinfish27@aol.com>
2023-11-16 17:08:37 -0700

    added universal route to index

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const { getCompliment, getFortune, receivePraise, becomeAllstar, deleteAllstar } = require('./controller');
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.get("/api/compliment", getCompliment);
 app.get("/api/fortune", getFortune)
 app.post("/api/praise", receivePraise)
